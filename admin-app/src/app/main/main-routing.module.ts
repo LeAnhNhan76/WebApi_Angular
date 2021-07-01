0308234b1544495798cef69605d7f273923a1a07
Nhan Le <lenhanasus4@gmail.com>
2021-07-01 23:29:21 +0700

    add main, home page

--- admin-app/src/app/main/main-routing.module.ts
+++ admin-app/src/app/main/main-routing.module.ts
@@ -4,16 +4,14 @@ import { MainComponent } from "./main.component";
 
 const routes : Routes = [
     {path: '', component: MainComponent, children:[
-        {path: '', component: MainComponent, children:[
-            {path: '', redirectTo: 'home', pathMatch: 'full'},
-            {path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
-            {path: 'function', loadChildren: () => import('./function/function.module').then((m) => m.FunctionModule)},
-            {path: 'product', loadChildren: () => import('./product/product.module').then((m) => m.ProductModule)},
-            {path: 'product-category', loadChildren: () => import('./product-category/product-category.module').then((m) => m.ProductCategoryModule)},
-            {path: 'user', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
-            {path: 'role', loadChildren: () => import('./role/role.module').then((m) => m.RoleModule)}
-        ]}
-    ] }
+        {path: '', redirectTo: 'home', pathMatch: 'full'},
+        {path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
+        {path: 'function', loadChildren: () => import('./function/function.module').then((m) => m.FunctionModule)},
+        {path: 'product', loadChildren: () => import('./product/product.module').then((m) => m.ProductModule)},
+        {path: 'product-category', loadChildren: () => import('./product-category/product-category.module').then((m) => m.ProductCategoryModule)},
+        {path: 'user', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
+        {path: 'role', loadChildren: () => import('./role/role.module').then((m) => m.RoleModule)}
+    ]}
 ]
 
 @NgModule({
