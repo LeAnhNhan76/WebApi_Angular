b4ffe55b4d74749c8acaf73a8a8a2d9e447f6cc2
Nhan Le <lenhanasus4@gmail.com>
2021-07-05 21:47:19 +0700

    update data service

--- admin-app/src/app/core/services/data.service.ts
+++ admin-app/src/app/core/services/data.service.ts
@@ -10,35 +10,29 @@ import { UtilityService } from './utility.service';
 @Injectable()
 export class DataService {
 
-  private _headers: HttpHeaders;
   constructor(private _httpClient: HttpClient
     , private _authenService : AuthenService
     , private _notificationService : NotificationService
     , private _utilityService : UtilityService ) { }
 
   get(uri: string): any {
-    let header = this.getHeader();
-    return this._httpClient.get(SystemConstants.BASE_API + uri, header).pipe(map((response: any) => response));
+    return this._httpClient.get(SystemConstants.BASE_API + uri, this.getHeader()).pipe(map((response: any) => response));
   }
 
   post(uri: string, data?: any): any {
-    let header = this.getHeader();
-    return this._httpClient.post(SystemConstants.BASE_API + uri, data, {headers: this._headers}).pipe(map((response: any) => response));
+    return this._httpClient.post(SystemConstants.BASE_API + uri, data, this.getHeader()).pipe(map((response: any) => response));
   }
 
   put(uri: string, data?: any): any {
-    let header = this.getHeader();
-    return this._httpClient.put(SystemConstants.BASE_API + uri, data, {headers: this._headers}).pipe(map((response: any) => response));
+    return this._httpClient.put(SystemConstants.BASE_API + uri, data, this.getHeader()).pipe(map((response: any) => response));
   }
 
   delete(uri: string, key: string, id: string): any{
-    let header = this.getHeader();
-    return this._httpClient.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, {headers: this._headers}).pipe(map((response: any) => response));
+    return this._httpClient.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, this.getHeader()).pipe(map((response: any) => response));
   }
 
   postFile(uri: string, data?: any): any {
-    let header = this.getHeader();
-    return this._httpClient.post(SystemConstants.BASE_API + uri, data, {headers: this._headers}).pipe(map((response: any) => response));
+    return this._httpClient.post(SystemConstants.BASE_API + uri, data, this.getHeader()).pipe(map((response: any) => response));
   }
 
   public handleError(error: any): any {
