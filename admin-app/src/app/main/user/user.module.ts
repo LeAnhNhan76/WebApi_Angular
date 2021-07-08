2e3405811929bb54e5182a752a511b5a97524755
Nhan Le <lenhanasus4@gmail.com>
2021-07-08 22:52:00 +0700

    show users list

--- admin-app/src/app/main/user/user.module.ts
+++ admin-app/src/app/main/user/user.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
+import { DataService, NotificationService, UtilityService } from 'src/app/core/services';
+import { FormsModule } from '@angular/forms';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -9,7 +13,15 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FormsModule,
+    PaginationModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [DataService, NotificationService, UtilityService],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
   ]
 })
 export class UserModule { }
